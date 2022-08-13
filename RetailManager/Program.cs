085ef0ee645b58f3a866144a93d47acc7a734c28
Soul-Remix <soulremix-dev@pm.me>
2022-08-13 22:04:44 +0300

    add bearer token to swagger
    Add the ability to use bearer token authorization using swagger UI

--- RetailManager/Program.cs
+++ RetailManager/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RetailManager.Data;
 using RetailManager.Interfaces;
 using RetailManager.Services;
@@ -14,16 +14,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Register Identity
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    // Specify identity user password rules
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequiredUniqueChars = 0;
-    options.Password.RequireDigit = false;
-    options.Password.RequireNonAlphanumeric = false;
-    
-    options.User.RequireUniqueEmail = true;
-}).AddEntityFrameworkStores<AppDbContext>()
+    {
+        // Specify identity user password rules
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequiredUniqueChars = 0;
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+
+        options.User.RequireUniqueEmail = true;
+    }).AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication(options =>
@@ -33,7 +33,7 @@ builder.Services.AddAuthentication(options =>
 }).AddJwtBearer(options =>
 {
     // TODO add a security key, validAudience and ValidIssuer
-    
+
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateAudience = true,
@@ -52,7 +52,38 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Swagger settings for bearer auth
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+    Name = "Authorization",
+    In = ParameterLocation.Header,
+    Type = SecuritySchemeType.Http,
+    Scheme = "bearer",
+    BearerFormat = "JWT"
+};
+
+var securityRequirement = new OpenApiSecurityRequirement
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "bearerAuth"
+            }
+        },
+        new string[] { }
+    }
+};
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("bearerAuth", securityScheme);
+    options.AddSecurityRequirement(securityRequirement);
+});
 
 var app = builder.Build();
 
